ddf0912661cc5e991646fd9babddf6d948797c4a
Raiondesu <a.raiondesu@gmail.com>
2019-03-25 09:31:07 +0300

    Update [all] move from root to ./packages/all

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  number: require('./packages/number'),
-  boolean: require('./packages/boolean'),
-  date: require('./packages/date'),
-  string: require('./packages/string'),
-};
---
+++ packages/all/lib/all.js
@@ -0,0 +1,6 @@
+module.exports = {
+  number: require('@js-random/number'),
+  boolean: require('@js-random/boolean'),
+  date: require('@js-random/date'),
+  string: require('@js-random/string'),
+};
