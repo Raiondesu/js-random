67ab4de0fa335e16e22502398b361ffc151eb3d2
Raiondesu <a.raiondesu@gmail.com>
2019-03-25 09:23:14 +0300

    Update [date] add documentation, repair range options

--- packages/date/lib/date.js
+++ packages/date/lib/date.js
@@ -4,8 +4,14 @@ module.exports = randomDate;
 
 var randomNumber = require('@js-random/number');
 
+/**
+ * Generates random dates based on a range
+ *
+ * @param {{ from?: Date, to?: Date }} range
+ * @returns
+ */
 function randomDate(range) {
-  range = typeof range === 'object' ? range : { from: 0, to: Number.MAX_SAFE_INTEGER };
+  range = typeof range === 'object' ? range : { from: new Date(0), to: new Date(Number.MAX_SAFE_INTEGER) };
 
-  return new Date(randomNumber(range.from, range.to));
+  return new Date(randomNumber(+range.from, +range.to));
 }
