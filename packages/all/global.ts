ca7a17f72be56599f9350253adb9703589357712
Raiondesu <a.raiondesu@gmail.com>
2019-03-26 16:15:24 +0300

    Update [all] global - remove explicit type imports

--- packages/all/global.ts
+++ packages/all/global.ts
@@ -1,8 +1,4 @@
 import '@js-random/number/lib/global';
-import '@js-random/number/types/global';
 import '@js-random/string/lib/global';
-import '@js-random/string/types/global';
 import '@js-random/boolean/lib/global';
-import '@js-random/boolean/types/global';
 import '@js-random/date/lib/global';
-import '@js-random/date/types/global';
